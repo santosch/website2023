1b11912c41fad9ecd727e652346ed61e345d3346
Sebastian Antosch <write@sebastian-antosch.de>
2023-02-25 16:30:29 +0100

    clean up header

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,60 +1,62 @@
 import styles from "./Hero.module.scss";
 import Image from "next/image";
-import {FaCodeBranch, FaFacebookF, FaInstagram, FaXing} from "react-icons/fa";
+import {FaFacebookF, FaInstagram, FaXing} from "react-icons/fa";
 
 export default function Hero(): JSX.Element {
     return (
         <header>
             <div className={styles.image}>
                 <div className={styles.glass}>
-                    <div className={styles.photo}>
-                        <Image
-                            src="/assets/images/sebastian-antosch.jpg"
-                            alt=""
-                            width="210"
-                            height="210"
-                        />
+                    <div className={styles.container}>
+                        <div className={styles.photoContainer}>
+                            <Image
+                                src="/assets/images/sebastian-antosch.jpg"
+                                alt=""
+                                width="210"
+                                height="210"
+                                className={styles.photo}
+                            />
+                        </div>
+                        <div className={styles.headlineContainer}>
+                            <h1 className={styles.headline}>
+                                Sebastian Antosch
+                            </h1>
+                            <h2 className={styles.subheadline}>
+                                Softwareentwickler • Javascript-Junkie • UI-Enthusiast
+                            </h2>
+                        </div>
                     </div>
-                    <div className={styles.social}>
-                        <a
-                            className={styles.socialIcon}
-                            href="https://www.xing.com/profile/Sebastian_Antosch/"
-                            title="Sebastian Antosch auf XING"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FaXing/>
-                        </a>
-                        <a
-                            className={styles.socialIcon}
-                            href="https://www.facebook.com/sebi.antosch/"
-                            title="Sebastian Antosch auf Facebook (Login erforderlich)"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FaFacebookF/>
-                        </a>
-                        <a
-                            className={styles.socialIcon}
-                            href="https://www.instagram.com/an.sebi/"
-                            title="Sebastian Antosch auf Instagram"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FaInstagram/>
-                        </a>
-                    </div>
+
                 </div>
-                <div className={styles.bottomGlass}>
-                    <h1>
-                        Sebastian Antosch
-                    </h1>
-                    <FaCodeBranch className={styles.branch1} />
-                    <FaCodeBranch className={styles.branch2} />
-                    <FaCodeBranch className={styles.branch3} />
-                    <FaCodeBranch className={styles.branch4} />
-                    <FaCodeBranch className={styles.branch5} />
-                </div>
+            </div>
+            <div className={styles.social}>
+                <a
+                    className={styles.socialIcon}
+                    href="https://www.xing.com/profile/Sebastian_Antosch/"
+                    title="Sebastian Antosch auf XING"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FaXing/>
+                </a>
+                <a
+                    className={styles.socialIcon}
+                    href="https://www.facebook.com/sebi.antosch/"
+                    title="Sebastian Antosch auf Facebook (Login erforderlich)"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FaFacebookF/>
+                </a>
+                <a
+                    className={styles.socialIcon}
+                    href="https://www.instagram.com/an.sebi/"
+                    title="Sebastian Antosch auf Instagram"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FaInstagram/>
+                </a>
             </div>
         </header>
     );
