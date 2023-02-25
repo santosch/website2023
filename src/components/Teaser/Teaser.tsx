53bfc92ed6b8c045550adcc2d197d2963ce1149d
Sebastian Antosch <write@sebastian-antosch.de>
2023-02-25 18:14:58 +0100

    finish teaser for desktop

--- src/components/Teaser/Teaser.tsx
+++ src/components/Teaser/Teaser.tsx
@@ -1,5 +1,5 @@
 import styles from "./Teaser.module.scss";
-import {FaFacebookF, FaInstagram, FaXing} from "react-icons/fa";
+import {FaEnvelope, FaGithub, FaXing} from "react-icons/fa";
 
 export default function Teaser(): JSX.Element {
     return (
@@ -20,37 +20,45 @@ export default function Teaser(): JSX.Element {
             <div className={styles.outerContainer}>
                 <div className={styles.container}>
 
-                </div>
-            </div>
+                    <div className={styles.social}>
+                        <a
+                            className={styles.socialIcon}
+                            href="https://www.xing.com/profile/Sebastian_Antosch/"
+                            title="Sebastian Antosch auf XING"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FaXing/>
+                        </a>
+                        <a
+                            className={styles.socialIcon}
+                            href="https://github.com/santosch"
+                            title="Sebastian Antosch auf GitHub"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FaGithub/>
+                        </a>
+                        <a
+                            className={styles.socialIcon}
+                            href="mailto:write@sebastian-antosch.de"
+                            title="Mail an Sebastian Antosch"
+                        >
+                            <FaEnvelope/>
+                        </a>
+                    </div>
 
-            <div className={styles.social}>
-                <a
-                    className={styles.socialIcon}
-                    href="https://www.xing.com/profile/Sebastian_Antosch/"
-                    title="Sebastian Antosch auf XING"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <FaXing/>
-                </a>
-                <a
-                    className={styles.socialIcon}
-                    href="https://www.facebook.com/sebi.antosch/"
-                    title="Sebastian Antosch auf Facebook (Login erforderlich)"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <FaFacebookF/>
-                </a>
-                <a
-                    className={styles.socialIcon}
-                    href="https://www.instagram.com/an.sebi/"
-                    title="Sebastian Antosch auf Instagram"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <FaInstagram/>
-                </a>
+                    <div className={styles.quote}>
+                        <h3 className={styles.quoteText}>
+                            Real quality means making sure that people are proud of the code they write,
+                            that they&apos;re involved and taking it personally.
+                        </h3>
+                        <div className={styles.quoteAuthor}>
+                            ~ Linus Torvalds, 2008
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
