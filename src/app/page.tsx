55b45f615cbf79f92f7529c75beec586539dc11e
Sebastian Antosch <write@sebastian-antosch.de>
2023-02-28 22:17:13 +0100

    remove old gitgraph stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import styles from './page.module.scss'
 import Teaser from "@santosch/components/Teaser/Teaser";
-import VitaGraph from "@santosch/components/VitaGraph/VitaGraph";
 import Vita from "@santosch/components/Vita/Vita";
 
 export default function Home() {
@@ -9,7 +8,6 @@ export default function Home() {
         <Teaser />
         <main className={styles.main}>
             <Vita />
-            <VitaGraph />
         </main>
       </>
   )
