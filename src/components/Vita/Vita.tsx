136561921611cf095ed2f69b78744d90f01a4944
Sebastian Antosch <write@sebastian-antosch.de>
2023-03-06 15:07:44 +0100

    variable based graph spacings

--- src/components/Vita/Vita.tsx
+++ src/components/Vita/Vita.tsx
@@ -27,208 +27,210 @@ import Birth from "./Stations/Other/Birth";
 export default function Vita(): JSX.Element {
 
     return (
-        <div>
-            <Graph>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s4={<Line color={COLORS.CHECK} />}
-                >
-                    <h2 className={styles.headline}>
-                        Beruflicher Werdegang
-                    </h2>
-                </Entry>
+        <div className={styles.container}>
+            <div className={styles.innerContainer}>
+                <Graph>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s4={<Line color={COLORS.CHECK} />}
+                    >
+                        <h2 className={styles.headline}>
+                            Beruflicher Werdegang
+                        </h2>
+                    </Entry>
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s4={<Line color={COLORS.CHECK} event="check-teamlead" />}
-                >
-                    <Check24 />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} event="isan" />}
-                    s4={<Line color={COLORS.CHECK}/>}
-                    spaceBefore={true}
-                >
-                    <Check24Teamlead />
-                </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s4={<Line color={COLORS.CHECK} event="check-teamlead" />}
+                    >
+                        <Check24 />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} event="isan" />}
+                        s4={<Line color={COLORS.CHECK}/>}
+                        spaceBefore={true}
+                    >
+                        <Check24Teamlead />
+                    </Entry>
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} tr={COLORS.ISAN} />}
-                    s2={<Line x={COLORS.ISAN} />}
-                    s3={<Line bl={COLORS.ISAN} />}
-                    s4={<Line color={COLORS.CHECK}/>}
-                />
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} tr={COLORS.ISAN} />}
+                        s2={<Line x={COLORS.ISAN} />}
+                        s3={<Line bl={COLORS.ISAN} />}
+                        s4={<Line color={COLORS.CHECK}/>}
+                    />
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s3={<Line color={COLORS.ISAN} />}
-                    s4={<Line color={COLORS.CHECK} event="check-senior"/>}
-                >
-                    <Check24Senior />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s3={<Line color={COLORS.ISAN} />}
-                    s4={<Line color={COLORS.CHECK} event="check-professional"/>}
-                    spaceBefore={true}
-                >
-                    <Check24Professional />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} br={COLORS.CHECK} />}
-                    s2={<Line x={COLORS.CHECK} />}
-                    s3={<Line color={COLORS.ISAN} x={COLORS.CHECK} />}
-                    s4={<Line tl={COLORS.CHECK}/>}
-                />
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s3={<Line color={COLORS.ISAN} />}
+                        s4={<Line color={COLORS.CHECK} event="check-senior"/>}
+                    >
+                        <Check24Senior />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s3={<Line color={COLORS.ISAN} />}
+                        s4={<Line color={COLORS.CHECK} event="check-professional"/>}
+                        spaceBefore={true}
+                    >
+                        <Check24Professional />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} br={COLORS.CHECK} />}
+                        s2={<Line x={COLORS.CHECK} />}
+                        s3={<Line color={COLORS.ISAN} x={COLORS.CHECK} />}
+                        s4={<Line tl={COLORS.CHECK}/>}
+                    />
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} event="crea" />}
-                    s3={<Line color={COLORS.ISAN}/>}
-                >
-                    <Crea />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} tr={COLORS.CREA} />}
-                    s2={<Line x={COLORS.CREA} />}
-                    s3={<Line color={COLORS.ISAN} x={COLORS.CREA} />}
-                    s4={<Line bl={COLORS.CREA}/>}
-                />
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s3={<Line color={COLORS.ISAN} />}
-                    s4={<Line color={COLORS.CREA} event="crea-webdev"/>}
-                >
-                    <CreaWebdev />
-                </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} event="crea" />}
+                        s3={<Line color={COLORS.ISAN}/>}
+                    >
+                        <Crea />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} tr={COLORS.CREA} />}
+                        s2={<Line x={COLORS.CREA} />}
+                        s3={<Line color={COLORS.ISAN} x={COLORS.CREA} />}
+                        s4={<Line bl={COLORS.CREA}/>}
+                    />
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s3={<Line color={COLORS.ISAN} />}
+                        s4={<Line color={COLORS.CREA} event="crea-webdev"/>}
+                    >
+                        <CreaWebdev />
+                    </Entry>
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} event="hsa" />}
-                    s3={<Line color={COLORS.ISAN}/>}
-                    s4={<Line color={COLORS.CREA}/>}
-                    spaceBefore={true}
-                >
-                    <CreaFreelancer />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} tr={COLORS.HSA} />}
-                    s2={<Line bl={COLORS.HSA} />}
-                    s3={<Line color={COLORS.ISAN}/>}
-                    s4={<Line color={COLORS.CREA}/>}
-                />
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s2={<Line color={COLORS.HSA} event="hsa-bachelor" />}
-                    s3={<Line color={COLORS.ISAN}/>}
-                    s4={<Line color={COLORS.CREA}/>}
-                >
-                    <CreaIntern />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s2={<Line color={COLORS.HSA} event="hsa-thesis" />}
-                    s3={<Line color={COLORS.ISAN}/>}
-                    s4={<Line color={COLORS.CREA}/>}
-                    spaceBefore={true}
-                >
-                    <Isan />
-                </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} event="hsa" />}
+                        s3={<Line color={COLORS.ISAN}/>}
+                        s4={<Line color={COLORS.CREA}/>}
+                        spaceBefore={true}
+                    >
+                        <CreaFreelancer />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} tr={COLORS.HSA} />}
+                        s2={<Line bl={COLORS.HSA} />}
+                        s3={<Line color={COLORS.ISAN}/>}
+                        s4={<Line color={COLORS.CREA}/>}
+                    />
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s2={<Line color={COLORS.HSA} event="hsa-bachelor" />}
+                        s3={<Line color={COLORS.ISAN}/>}
+                        s4={<Line color={COLORS.CREA}/>}
+                    >
+                        <CreaIntern />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s2={<Line color={COLORS.HSA} event="hsa-thesis" />}
+                        s3={<Line color={COLORS.ISAN}/>}
+                        s4={<Line color={COLORS.CREA}/>}
+                        spaceBefore={true}
+                    >
+                        <Isan />
+                    </Entry>
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s2={<Line color={COLORS.HSA} />}
-                    s3={<Line color={COLORS.ISAN}/>}
-                    s4={<Line color={COLORS.CREA} event="crea-freelancer" />}
-                    spaceBefore={true}
-                >
-                    <IsanDev />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s2={<Line color={COLORS.HSA} />}
-                    s3={<Line color={COLORS.ISAN}/>}
-                    s4={<Line color={COLORS.CREA} event="crea-intern" />}
-                    spaceBefore={true}
-                >
-                    <IsanFounding />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} br={COLORS.CREA} />}
-                    s2={<Line color={COLORS.HSA} x={COLORS.CREA} />}
-                    s3={<Line color={COLORS.ISAN} x={COLORS.CREA} />}
-                    s4={<Line tl={COLORS.CREA} />}
-                />
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s2={<Line color={COLORS.HSA} />}
+                        s3={<Line color={COLORS.ISAN}/>}
+                        s4={<Line color={COLORS.CREA} event="crea-freelancer" />}
+                        spaceBefore={true}
+                    >
+                        <IsanDev />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s2={<Line color={COLORS.HSA} />}
+                        s3={<Line color={COLORS.ISAN}/>}
+                        s4={<Line color={COLORS.CREA} event="crea-intern" />}
+                        spaceBefore={true}
+                    >
+                        <IsanFounding />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} br={COLORS.CREA} />}
+                        s2={<Line color={COLORS.HSA} x={COLORS.CREA} />}
+                        s3={<Line color={COLORS.ISAN} x={COLORS.CREA} />}
+                        s4={<Line tl={COLORS.CREA} />}
+                    />
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s2={<Line color={COLORS.HSA} event="hsa-bachelor" />}
-                    s3={<Line color={COLORS.ISAN} />}
-                >
-                    <h2 className={styles.headline}>
-                        Schulische Ausbildung
-                    </h2>
-                    <Hsa />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} br={COLORS.HSA} />}
-                    s2={<Line tl={COLORS.HSA} />}
-                    s3={<Line color={COLORS.ISAN} />}
-                />
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s2={<Line color={COLORS.HSA} event="hsa-bachelor" />}
+                        s3={<Line color={COLORS.ISAN} />}
+                    >
+                        <h2 className={styles.headline}>
+                            Schulische Ausbildung
+                        </h2>
+                        <Hsa />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} br={COLORS.HSA} />}
+                        s2={<Line tl={COLORS.HSA} />}
+                        s3={<Line color={COLORS.ISAN} />}
+                    />
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} event="school"/>}
-                    s3={<Line color={COLORS.ISAN} />}
-                >
-                    <HsaBachelor />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} tr={COLORS.SCHOOL} />}
-                    s2={<Line bl={COLORS.SCHOOL} />}
-                    s3={<Line color={COLORS.ISAN} />}
-                />
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s2={<Line color={COLORS.SCHOOL} event="school-highschool" />}
-                    s3={<Line color={COLORS.ISAN} />}
-                >
-                    <HsaThesis />
-                </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} event="school"/>}
+                        s3={<Line color={COLORS.ISAN} />}
+                    >
+                        <HsaBachelor />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} tr={COLORS.SCHOOL} />}
+                        s2={<Line bl={COLORS.SCHOOL} />}
+                        s3={<Line color={COLORS.ISAN} />}
+                    />
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s2={<Line color={COLORS.SCHOOL} event="school-highschool" />}
+                        s3={<Line color={COLORS.ISAN} />}
+                    >
+                        <HsaThesis />
+                    </Entry>
 
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s2={<Line color={COLORS.SCHOOL} />}
-                    s3={<Line color={COLORS.ISAN} event="isan" />}
-                />
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} br={COLORS.ISAN} />}
-                    s2={<Line color={COLORS.SCHOOL} x={COLORS.ISAN} />}
-                    s3={<Line tl={COLORS.ISAN} />}
-                />
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s2={<Line color={COLORS.SCHOOL} />}
+                        s3={<Line color={COLORS.ISAN} event="isan" />}
+                    />
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} br={COLORS.ISAN} />}
+                        s2={<Line color={COLORS.SCHOOL} x={COLORS.ISAN} />}
+                        s3={<Line tl={COLORS.ISAN} />}
+                    />
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s2={<Line color={COLORS.SCHOOL} event="school-highschool" />}
-                >
-                    <SchoolGymnasium />
-                </Entry>
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} />}
-                    s2={<Line color={COLORS.SCHOOL} event="school-elementary" />}
-                    spaceBefore={true}
-                >
-                    <SchoolElementary />
-                </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s2={<Line color={COLORS.SCHOOL} event="school-highschool" />}
+                    >
+                        <SchoolGymnasium />
+                    </Entry>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} />}
+                        s2={<Line color={COLORS.SCHOOL} event="school-elementary" />}
+                        spaceBefore={true}
+                    >
+                        <SchoolElementary />
+                    </Entry>
 
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} br={COLORS.SCHOOL} />}
-                    s2={<Line tl={COLORS.SCHOOL} />}
-                />
-                <Entry
-                    s1={<Line color={COLORS.DEFAULT} event="birth" />}
-                >
-                    <Birth />
-                </Entry>
-            </Graph>
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} br={COLORS.SCHOOL} />}
+                        s2={<Line tl={COLORS.SCHOOL} />}
+                    />
+                    <Entry
+                        s1={<Line color={COLORS.DEFAULT} event="birth" />}
+                    >
+                        <Birth />
+                    </Entry>
+                </Graph>
+            </div>
         </div>
     );
 }
