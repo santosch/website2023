fa2fdfaee6caa83306f553d838062817e3d284c8
Sebastian Antosch <write@sebastian-antosch.de>
2023-05-09 16:33:06 +0200

    nicer 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,25 @@
 import Hero from "@santosch/components/Hero/Hero";
+import styles from "./not-found.module.scss";
+import Image from "next/image";
 
 export default function NotFound() {
     return (
         <>
             <Hero />
-            <section>
-                <h2>Not Found MEOW</h2>
-                <p>Could not find requested resource</p>
+            <section className={styles.section}>
+                <div className={styles.text}>
+                    <h2 className={styles.headline}>404</h2>
+                    <p>Page not found - take a flower instead!</p>
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image
+                        src="/assets/images/404image.png"
+                        alt="Flower"
+                        width={800}
+                        height={800}
+                        className={styles.img}
+                    />
+                </div>
             </section>
         </>
     );
