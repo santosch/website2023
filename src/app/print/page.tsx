d1a8c41be89f92492fa6766270abf72890ddd1fd
Sebastian Antosch <write@sebastian-antosch.de>
2023-05-10 20:15:28 +0200

    extend print page with languages

--- src/app/print/page.tsx
+++ src/app/print/page.tsx
@@ -3,6 +3,7 @@ import Vita from "@santosch/components/Vita/Vita";
 import CoursesAndAchievements from "@santosch/components/CoursesAndAchievements/CoursesAndAchievements";
 import Image from "next/image";
 import classNames from "classnames";
+import {FaEnvelope, FaGlobe, FaHome, FaPassport, FaPhone} from "react-icons/fa";
 
 export default function Print(): JSX.Element {
     return (
@@ -32,8 +33,76 @@ export default function Print(): JSX.Element {
             </div>
             <div className={styles.grid}>
                 <div className={styles.aside}>
-                    Test1
+
+                    <div className={styles.entry}>
+                        <a href="mailto:sebastian.antosch@gmail.com">
+                            sebastian.antosch@gmail.com
+                        </a>
+                        <div className={styles.icon}>
+                            <FaEnvelope />
+                        </div>
+                    </div>
+
+                    <div className={styles.entry}>
+                        <a href="tel:004915788562613">
+                            +49 1578 8562613
+                        </a>
+                        <div className={styles.icon}>
+                            <FaPhone />
+                        </div>
+                    </div>
+
+                    <div className={styles.entry}>
+                        <a href="https://sebastian-antosch.de">
+                            https://sebastian-antosch.de
+                        </a>
+                        <div className={styles.icon}>
+                            <FaGlobe />
+                        </div>
+                    </div>
+
+                    <div className={styles.entry}>
+                        Stettenstraße 17<br/>
+                        86150 Augsburg
+                        <div className={styles.icon}>
+                            <FaHome />
+                        </div>
+                    </div>
+
+                    <div className={styles.entry}>
+                        Staatsangehörigkeit: Deutsch
+                        <div className={styles.icon}>
+                            <FaPassport />
+                        </div>
+                    </div>
+
+                    <h3 className={styles.sidebarHeadline}>
+                        Sprachkenntnisse
+                    </h3>
+
+                    {[
+                        {label: 'Deutsch: Muttersprache', val: '100%'},
+                        {label: 'Englisch: Fließend', val: '80%'},
+                        {label: 'Französisch: Grundlagen', val: '30%'},
+                    ].map((language) => (
+                        <div
+                            className={styles.language}
+                            key={language.label}
+                        >
+                            {language.label}
+                            <div className={styles.bar}>
+                                <div
+                                    className={styles.barFill}
+                                    style={{width: language.val}}
+                                />
+                            </div>
+                        </div>
+                    ))}
+
                     <div className={styles.pagebreak}></div>
+
+
+
                     Test2
                 </div>
                 <div className={styles.main} style={{marginTop: -84}}>
@@ -49,7 +118,9 @@ export default function Print(): JSX.Element {
                             </h2>
                         )}
                     />
-                    <h2 className={styles.headline}>
+
+                    <div className={styles.pagebreak}></div>
+                    <h2 className={styles.headline} style={{marginTop: 40, marginBottom: -20}}>
                         Weiterbildungen
                     </h2>
                     <CoursesAndAchievements />
